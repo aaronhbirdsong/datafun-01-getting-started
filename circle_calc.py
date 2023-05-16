610cca2d76e9afe90b5ae22d769a4046d2fb8a4f
Aaron <aaronhbirdsong@gmail.com>
2023-05-16 08:20:58 -0500

    message

--- circle_calc.py
+++ circle_calc.py
@@ -53,3 +53,82 @@ logger.info("Much better!")
 # Use built-in open() function to read log file and print it to the terminal
 with open(logname, 'r') as file_wrapper:
     print(file_wrapper.read())
+
+string1 = input(5)
+
+string2 = input(10)
+
+string3 = input(15)
+
+# Assuming these are your three values
+a = 10
+b = 20
+c = 30
+
+# Calculate the needed values
+_sum = a + b + c
+_average = _sum / 3
+_product = a * b * c
+_minimum = min(a, b, c)
+_maximum = max(a, b, c)
+
+# Print the results using f-strings
+print(f"The sum of the values is {_sum}")
+print(f"The average of the values is {_average}")
+print(f"The product of the values is {_product}")
+print(f"The smallest value is {_minimum}")
+print(f"The largest value is {_maximum}")
+
+In [7]: # Assuming these are your three values
+   ...: a = 10
+   ...: b = 20
+   ...: c = 30
+   ...:
+   ...: # Calculate the needed values
+   ...: _sum = a + b + c
+   ...: _average = _sum / 3
+   ...: _product = a * b * c
+   ...: _minimum = min(a, b, c)
+   ...: _maximum = max(a, b, c)
+   ...:
+   ...: # Print the results using f-strings
+   ...: print(f"The sum of the values is {_sum}")
+   ...: print(f"The average of the values is {_average}")
+   ...: print(f"The product of the values is {_product}")
+   ...: print(f"The smallest value is {_minimum}")
+   ...: print(f"The largest value is {_maximum}")
+The sum of the values is 60
+The average of the values is 20.0
+The product of the values is 6000
+The smallest value is 10
+The largest value is 30
+
+In [8]: import logging
+   ...:
+   ...: # Assuming these are your three values
+   ...: a = 10
+   ...: b = 20
+   ...: c = 30
+   ...:
+   ...: # Calculate the needed values
+   ...: _sum = a + b + c
+   ...: _average = _sum / 3
+   ...: _product = a * b * c
+   ...: _minimum = min(a, b, c)
+   ...: _maximum = max(a, b, c)
+   ...:
+   ...: # Set up basic logging (to the console, with level INFO)
+   ...: logging.basicConfig(level=logging.INFO)
+   ...:
+   ...: # Log the results
+   ...: logging.info(f"The sum of the values is {_sum}")
+   ...: logging.info(f"The average of the values is {_average}")
+   ...: logging.info(f"The product of the values is {_product}")
+   ...: logging.info(f"The smallest value is {_minimum}")
+   ...: logging.info(f"The largest value is {_maximum}")
+INFO:root:The sum of the values is 60
+INFO:root:The average of the values is 20.0
+INFO:root:The product of the values is 6000
+INFO:root:The smallest value is 10
+INFO:root:The largest value is 30
+
